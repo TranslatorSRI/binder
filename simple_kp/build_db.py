050ff56f06a9d9d1bf9bfb4c61b42d360944713c
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-05-27 15:25:46 -0400

    Remove file data upload and dependency on small-kg

--- simple_kp/build_db.py
+++ simple_kp/build_db.py
@@ -6,9 +6,6 @@ import uuid
 
 import aiosqlite
 
-from small_kg import mychem, synonyms_file
-from ._types import CURIEMap
-
 
 async def get_data_from_string(data: str):
     """Get data from string.
@@ -70,77 +67,13 @@ async def get_data_from_string(data: str):
     return list(nodes.values()), list(edges.values())
 
 
-async def get_data_from_files(
-        curie_prefixes: CURIEMap = None,
-        nodes_file=mychem.nodes_file,
-        edges_file=mychem.edges_file,
-):
-    """Get data from files."""
-    with open(nodes_file, newline="", encoding="utf-8-sig") as csvfile:
-        reader = csv.DictReader(
-            csvfile,
-            delimiter=',',
-        )
-        nodes = list(reader)
-    with open(edges_file, newline="", encoding="utf-8-sig") as csvfile:
-        reader = csv.DictReader(
-            csvfile,
-            delimiter=',',
-        )
-        edges = list(reader)
-    edges = [
-        {
-            **edge,
-            "id": idx
-        }
-        for idx, edge in enumerate(edges)
-    ]
-
-    if curie_prefixes is not None:
-        # map to synonyms with prefix
-        with open(synonyms_file, newline="") as csvfile:
-            reader = csv.reader(
-                csvfile,
-                delimiter=',',
-            )
-            synsets = list(reader)
-        synset_map = {
-            term: synset
-            for synset in synsets
-            for term in synset
-        }
-        node_map = dict()
-        for node in nodes:
-            # get preferred CURIE
-            for curie_prefix in curie_prefixes[node["category"]]:
-                # get CURIE with prefix, if one exists
-                try:
-                    node_map[node["id"]] = next(
-                        curie
-                        for curie in synset_map[node["id"]]
-                        if curie.startswith(curie_prefix + ":")
-                    )
-                    break
-                except StopIteration:
-                    continue
-        for node in nodes:
-            node["id"] = node_map.get(node["id"], node["id"])
-        for edge in edges:
-            edge["subject"] = node_map.get(edge["subject"], edge["subject"])
-            edge["object"] = node_map.get(edge["object"], edge["object"])
-    return nodes, edges
-
-
 async def add_data(
         connection: aiosqlite.Connection,
-        data: str = None,
+        data: str,
         **kwargs,
 ):
     """Add data to SQLite database."""
-    if data is not None:
-        nodes, edges = await get_data_from_string(data)
-    else:
-        nodes, edges = await get_data_from_files(**kwargs)
+    nodes, edges = await get_data_from_string(data)
 
     if nodes:
         # Convert category list to our custom string format
