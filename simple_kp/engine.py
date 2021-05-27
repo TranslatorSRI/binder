e51d6a8f4dd0d1e3d11555c494a8fd6e6b21fb59
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-05-27 11:26:46 -0400

    Restructure
    * allow bidirectional lookups always
    * support loops

--- simple_kp/engine.py
+++ simple_kp/engine.py
@@ -1,5 +1,6 @@
 """SQL query graph engine."""
 from collections import defaultdict
+import copy
 import itertools
 import logging
 import os
@@ -9,7 +10,8 @@ from typing import Any, Dict, Tuple, Union
 
 import aiosqlite
 
-from .util import is_cyclic, to_list, validate_edge, validate_node, NoAnswersException
+from .graph import Graph
+from .util import to_list, NoAnswersException
 
 LOGGER = logging.getLogger(__name__)
 
@@ -21,25 +23,6 @@ def normalize_qgraph(qgraph):
     for edge in qgraph["edges"].values():
         edge["predicate"] = to_list(
             edge.get("predicate", "biolink:related_to"))
-
-
-def to_kedge(row):
-    """Convert operation to kedge."""
-    row.pop("id")
-    if row["predicate"].startswith("<"):
-        kedge = {
-            "subject": row.pop("target"),
-            "predicate": row.pop("predicate")[2:-1],
-            "object": row.pop("source"),
-        }
-    else:
-        kedge = {
-            "subject": row.pop("source"),
-            "predicate": row.pop("predicate")[1:-2],
-            "object": row.pop("target"),
-        }
-    kedge.update(row)
-    return kedge
 
 
 def custom_row_factory(cursor, row):
@@ -112,8 +95,8 @@ class KnowledgeProvider():
 
         ops = set()
         for edge in edges:
-            source_node = nodes[edge["source"]]
-            target_node = nodes[edge["target"]]
+            subject_node = nodes[edge["subject"]]
+            object_node = nodes[edge["object"]]
 
             operation_iterator = itertools.product(
                 [subject_node["category"]],
@@ -125,9 +108,9 @@ class KnowledgeProvider():
 
         return [
             {
-                "source_type": op[0],
-                "edge_type": op[1],
-                "target_type": op[2],
+                "subject": op[0],
+                "predicate": op[1],
+                "object": op[2],
             }
             for op in ops
         ]
@@ -148,7 +131,7 @@ class KnowledgeProvider():
         }
 
     async def get_kedges(self, **kwargs):
-        """Get kedges by source id."""
+        """Get kedges."""
         assert kwargs
         conditions = []
         for key, value in kwargs.items():
@@ -159,7 +142,12 @@ class KnowledgeProvider():
                 conditions.append(f"{key} = ?")
         conditions = " AND ".join(conditions)
         async with self.db.execute(
-                "SELECT id, source, predicate, target FROM edges WHERE " + conditions,
+                (
+                    "SELECT edge.id AS id, subject.id AS subject, edge.predicate as predicate, object.id as object "
+                    "FROM edges AS edge "
+                    "JOIN nodes AS subject ON edge.subject = subject.id "
+                    "JOIN nodes AS object ON edge.object = object.id "
+                 ) + "WHERE " + conditions,
                 list(
                     x for value in kwargs.values()
                     for x in to_list(value)
@@ -168,108 +156,106 @@ class KnowledgeProvider():
             rows = await cursor.fetchall()
 
         return {
-            row["id"]: to_kedge(dict(row))
+            row["id"]: {key: value for key, value in row.items() if key != "id"}
             for row in rows
         }
 
-    async def expand_from_node(
+    async def lookup(
             self,
-            qgraph: Dict[str, Any],
-            qnode_id: str,
-            knode_id: str,
-            knode: Dict[str, Any],
+            qgraph: Graph,
     ):
         """Expand from query graph node."""
+        LOGGER.debug(f"Lookup for qgraph: {qgraph}")
         # if this is a leaf node, we're done
         if not qgraph["edges"]:
-            return {
-                "nodes": {
-                    knode_id: knode
-                },
-                "edges": dict(),
-            }, [{
-                "node_bindings": {
-                    qnode_id: [{
-                        "id": knode_id,
-                    }],
-                },
-                "edge_bindings": dict(),
-            }]
-
-        LOGGER.debug(
-            "Expanding from node %s/%s...",
-            qnode_id,
-            knode_id,
-        )
-
+            return {"nodes": dict(), "edges": dict()}, [{"node_bindings": dict(), "edge_bindings": dict()}]
         kgraph = {"nodes": dict(), "edges": dict()}
         results = []
-        for qedge_id, qedge in qgraph["edges"].items():
+        source_qnode_id, source_qnode = next(
+            (key, qnode)
+            for key, qnode in qgraph["nodes"].items()
+            if qnode.get("id", None) is not None
+        )
+
+        # look up associated knode(s)
+        curies = to_list(source_qnode["id"])
+        for curie in curies:
+            source_knode_id, source_knode = await self.get_knode(curie)
+            LOGGER.debug(
+                "Expanding from node %s/%s...",
+                source_qnode_id,
+                source_knode_id,
+            )
+
+            qgraph_ = copy.deepcopy(qgraph)
+            qedge_id, qedge = next(
+                (qedge_id, qedge)
+                for qedge_id, qedge in list(qgraph["edges"].items())
+                if source_qnode_id in (qedge["subject"], qedge["object"])
+                and qgraph_["edges"].pop(qedge_id) is not None
+            )
+
             # get kedges for qedge
-            if qnode_id == qedge["subject"]:
-                kedges = await self.get_kedges(
-                    source=knode_id,
-                    predicate=[
-                        f"-{predicate}->"
-                        for predicate in qedge["predicate"]
-                    ],
-                )
-            elif qnode_id == qedge["object"]:
-                kedges = await self.get_kedges(
-                    source=knode_id,
-                    predicate=[
-                        f"<-{predicate}-"
-                        for predicate in qedge["predicate"]
-                    ],
-                )
-            else:
-                continue
+            kwargs = dict()
+            for key, value in qedge.items():
+                if key in ("subject", "object"):
+                    continue
+                kwargs[f"edge.{key}"] = value
+            for role in ("subject", "object"):
+                for key, value in qgraph_["nodes"][qedge[role]].items():
+                    if key in ():
+                        continue
+                    kwargs[f"{role}.{key}"] = value
+
+            kedges = await self.get_kedges(**kwargs)
 
             for kedge_id, kedge in kedges.items():
-                # validate kedge against qedge
-                if not validate_edge(qedge, kedge):
-                    LOGGER.debug(
-                        "kedge %s does not satisfy qedge %s",
-                        str(kedge),
-                        str(qedge),
-                    )
-                    continue
-
-                # recursively expand from edge
-                kgraph_, results_ = await self.expand_from_edge(
-                    {
-                        "nodes": {
-                            key: value
-                            for key, value in qgraph["nodes"].items()
-                            if key != qnode_id
-                        },
-                        "edges": qgraph["edges"],
-                    },
+                LOGGER.debug(
+                    "Expanding along edge %s/%s...",
                     qedge_id,
                     kedge_id,
-                    kedge,
                 )
+                # now solve the smaller question
+
+                qgraph__ = copy.deepcopy(qgraph_)
+                # pin node
+                qgraph__["nodes"][qedge["subject"]]["id"] = [kedge["subject"]]
+                qgraph__["nodes"][qedge["object"]]["id"] = [kedge["object"]]
+                # remove orphaned nodes
+                qgraph__.remove_orphaned()
+                
+                kgraph_, results_ = await self.lookup(qgraph__)
+
+                # add edge to results and kgraph
+                kgraph["edges"][kedge_id] = kedge
+                subject_knode_id, subject_knode = await self.get_knode(kedge["subject"])
+                object_knode_id, object_knode = await self.get_knode(kedge["object"])
+                kgraph["nodes"][subject_knode_id] = subject_knode
+                kgraph["nodes"][object_knode_id] = object_knode
+                results_ = [
+                    {
+                        "node_bindings": {
+                            **result["node_bindings"],
+                            qedge["subject"]: [{
+                                "id": kedge["subject"],
+                            }],
+                            qedge["object"]: [{
+                                "id": kedge["object"],
+                            }],
+                        },
+                        "edge_bindings": {
+                            **result["edge_bindings"],
+                            qedge_id: [{
+                                "id": kedge_id,
+                            }],
+                        },
+                    }
+                    for result in results_
+                ]
                 kgraph["nodes"].update(kgraph_["nodes"])
                 kgraph["edges"].update(kgraph_["edges"])
                 results.extend(results_)
 
-        if not results:
-            return kgraph, results
-
-        # add node to results and kgraph
-        kgraph["nodes"][knode_id] = knode
-        results = [
-            {
-                "node_bindings": {
-                    **result["node_bindings"],
-                    qnode_id: [{
-                        "id": knode_id,
-                    }],
-                },
-                "edge_bindings": result["edge_bindings"],
-            }
-            for result in results
-        ]
         return kgraph, results
 
     async def get_knode(self, knode_id: str) -> Tuple[str, Dict]:
@@ -283,111 +269,9 @@ class KnowledgeProvider():
             raise NoAnswersException()
         return row["id"], {k: v for k, v in dict(row).items() if k != "id"}
 
-    async def expand_from_edge(
-            self,
-            qgraph: Dict[str, Any],
-            qedge_id: str,
-            kedge_id: str,
-            kedge: Dict[str, Any],
-    ):
-        """Expand along a query graph edge.
-
-        Only one endpoint should be present in the query graph.
-        """
-        LOGGER.debug(
-            "Expanding from edge %s/%s...",
-            qedge_id,
-            kedge_id,
-        )
-        qedge = qgraph["edges"][qedge_id]
-
-        # get the remaining endpoint (query-graph and knowledge-graph nodes)
-        if qedge["object"] in qgraph["nodes"]:
-            qnode_id = qedge["object"]
-            knode_id, knode = await self.get_knode(kedge["object"])
-        elif qedge["subject"] in qgraph["nodes"]:
-            qnode_id = qedge["subject"]
-            knode_id, knode = await self.get_knode(kedge["subject"])
-        else:
-            raise RuntimeError("Expanding from qedge with no endpoints?")
-        qnode = qgraph["nodes"][qnode_id]
-
-        kgraph = {"nodes": dict(), "edges": dict()}
-        results = []
-
-        # validate knode against qnode
-        _knode = {**knode, "id": knode_id}
-        if not validate_node(qnode, _knode):
-            LOGGER.debug(
-                "knode %s does not satisfy qnode %s",
-                str(_knode),
-                str(qnode),
-            )
-            return kgraph, results
-
-        # recursively expand from the endpoint
-        kgraph, results = await self.expand_from_node(
-            {
-                "nodes": qgraph["nodes"],
-                "edges": {
-                    key: value
-                    for key, value in qgraph["edges"].items()
-                    if key != qedge_id
-                },
-            },
-            qnode_id,
-            knode_id,
-            knode,
-        )
-        if not results:
-            return kgraph, results
-
-        # add edge to results and kgraph
-        kgraph["edges"][kedge_id] = kedge
-        results = [
-            {
-                "node_bindings": result["node_bindings"],
-                "edge_bindings": {
-                    **result["edge_bindings"],
-                    qedge_id: [{
-                        "id": kedge_id,
-                    }],
-                },
-            }
-            for result in results
-        ]
-        return kgraph, results
-
     async def get_results(self, qgraph: Dict[str, Any]):
         """Get results and kgraph."""
-        if is_cyclic(qgraph):
-            raise ValueError("Query graph is cyclic.")
+        qgraph = Graph(qgraph)
         normalize_qgraph(qgraph)
-        # find fixed qnode
-        qnode_id, qnode = next(
-            (key, qnode)
-            for key, qnode in qgraph["nodes"].items()
-            if qnode.get("id", None) is not None
-        )
-        # look up associated knode(s)
-        curies = to_list(qnode["id"])
-        kgraph = {
-            "nodes": dict(),
-            "edges": dict(),
-        }
-        results = []
-        for curie in curies:
-            try:
-                knode_id, knode = await self.get_knode(curie)
-            except NoAnswersException:
-                break
-            kgraph_, results_ = await self.expand_from_node(
-                qgraph,
-                qnode_id,
-                knode_id,
-                knode,
-            )
-            kgraph["nodes"].update(kgraph_["nodes"])
-            kgraph["edges"].update(kgraph_["edges"])
-            results.extend(results_)
+        kgraph, results = await self.lookup(qgraph)
         return kgraph, results
