5b9526eb1cec618f8b8976014adf9dd868313577
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-06-28 13:54:41 -0400

    Add util tests

---
+++ tests/test_util.py
@@ -0,0 +1,32 @@
+"""Test utilities."""
+from simple_kp.util import get_subcategories, get_subpredicates
+from simple_kp.engine import normalize_qgraph
+
+
+def test_expand_category():
+    """Test get_subcategories()."""
+    categories = get_subcategories("biolink:DiseaseOrPhenotypicFeature")
+    assert isinstance(categories, list)
+    assert len(categories) >= 3
+    categories = get_subcategories("biolink:Disease")
+    assert isinstance(categories, list)
+    assert len(categories) == 1
+
+
+def test_expand_predicate():
+    """Test get_subpredicates()."""
+    predicates = get_subpredicates("biolink:affects_expression_of")
+    assert isinstance(predicates, list)
+    assert len(predicates) == 3
+
+
+def test_normalize_qgraph():
+    """Test normalize_qgraph()."""
+    qgraph = {
+        "nodes": {
+            "something": {},
+        },
+        "edges": {},
+    }
+    normalize_qgraph(qgraph)
+    assert qgraph["nodes"]["something"]["categories"]
