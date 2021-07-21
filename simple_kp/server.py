5fa90d1d92c76672c1147582564ecf646e3ba842
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-07-21 08:54:03 -0400

    Bump version

--- simple_kp/server.py
+++ simple_kp/server.py
@@ -11,7 +11,7 @@ LOGGER = logging.getLogger(__name__)
 app = FastAPI(
     title="Test KP",
     description="Simple dummy KP for testing",
-    version="2.0.0",
+    version="2.1.0",
 )
 
 database_files = glob.glob("./*.db")
