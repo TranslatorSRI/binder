d6438eea12fe33055c40e028d1c722ec689d3ace
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-07-26 09:58:09 -0400

    Add operations tests

---
+++ tests/test_server.py
@@ -0,0 +1,88 @@
+"""Test server."""
+import httpx
+import pytest
+
+from simple_kp.testing import kp_overlay
+
+from tests.logging_setup import setup_logger
+
+setup_logger()
+
+
+@kp_overlay("kp", data="""
+    MONDO:0005148(( category biolink:Disease ))
+    MONDO:0005148<-- predicate biolink:treats --CHEBI:6801
+    CHEBI:6801(( category biolink:ChemicalSubstance ))
+    """
+)
+@pytest.mark.asyncio
+async def test_unsupported_operation():
+    """Test unsupported operation."""
+    request = {
+        "message": {
+            "query_graph": {
+                "nodes": {
+                    "n0": {
+                        "categories": ["biolink:ChemicalSubstance"],
+                    },
+                    "n1": {
+                        "categories": ["biolink:Disease"],
+                        "ids": ["MONDO:0005148"],
+                    },
+                },
+                "edges": {
+                    "e01": {
+                        "subject": "n0",
+                        "object": "n1",
+                        "predicates": ["biolink:treats"],
+                    },
+                },
+            }
+        },
+        "workflow": [
+            "restate",
+        ],
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post("http://kp/query", json=request)
+    assert response.status_code == 400
+
+
+@kp_overlay("kp", data="""
+    MONDO:0005148(( category biolink:Disease ))
+    MONDO:0005148<-- predicate biolink:treats --CHEBI:6801
+    CHEBI:6801(( category biolink:ChemicalSubstance ))
+    """
+)
+@pytest.mark.asyncio
+async def test_lookup():
+    """Test lookup."""
+    request = {
+        "message": {
+            "query_graph": {
+                "nodes": {
+                    "n0": {
+                        "categories": ["biolink:ChemicalSubstance"],
+                    },
+                    "n1": {
+                        "categories": ["biolink:Disease"],
+                        "ids": ["MONDO:0005148"],
+                    },
+                },
+                "edges": {
+                    "e01": {
+                        "subject": "n0",
+                        "object": "n1",
+                        "predicates": ["biolink:treats"],
+                    },
+                },
+            }
+        },
+        "workflow": [
+            "lookup",
+        ],
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post("http://kp/query", json=request)
+    assert response.status_code == 200
+    assert response.json()["message"]["results"]
