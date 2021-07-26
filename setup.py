261f976d826359530075947c68683c0636467ba0
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-07-26 09:21:09 -0400

    Add library requirements

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ setup(
     description='Translator KP Registry',
     packages=['simple_kp'],
     include_package_data=True,
+    install_requires=[
+        "aiosqlite>=0.16.0",
+        "bmt-lite-1.8.2>=1.0.2,<2.0",
+        "fastapi>=0.65.2",
+        "reasoner-pydantic>=1.1.2.1,<1.1.3",
+    ],
     zip_safe=False,
     license='MIT',
     python_requires='>=3.6',
