116502d06f586ee574ac6dcbceb79c29f7d092ad
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-07-26 13:17:54 -0400

    Use double quotes

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name='binder',
-    version='3.0.0',
-    author='Patrick Wang',
-    author_email='patrick@covar.com',
-    url='https://github.com/TranslatorSRI/binder',
-    description='Translator *graph binder',
-    packages=['binder'],
+    name="binder",
+    version="3.0.0",
+    author="Patrick Wang",
+    author_email="patrick@covar.com",
+    url="https://github.com/TranslatorSRI/binder",
+    description="Translator *graph binder",
+    packages=["binder"],
     include_package_data=True,
     install_requires=[
         "aiosqlite>=0.16.0",
@@ -17,6 +17,6 @@ setup(
         "reasoner-pydantic>=1.1.2.1,<1.1.3",
     ],
     zip_safe=False,
-    license='MIT',
-    python_requires='>=3.6',
+    license="MIT",
+    python_requires=">=3.6",
 )
