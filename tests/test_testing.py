c33222578e7eca9eb3c3dcc3c049734ba9fd6258
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-07-26 10:39:02 -0400

    Add KnowledgeProvider(filename) test

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,7 +1,12 @@
 """Test testing."""
+from simple_kp.engine import KnowledgeProvider
+import tempfile
+
+import aiosqlite
 import httpx
 import pytest
 
+from simple_kp.build_db import add_data
 from simple_kp.testing import kp_overlay
 
 from tests.logging_setup import setup_logger
@@ -43,3 +48,41 @@ async def test_overlay():
     async with httpx.AsyncClient() as client:
         response = await client.post("http://kp/query", json=request)
     response.raise_for_status()
+
+
+@pytest.mark.asyncio
+async def test_database_file():
+    """Test database file."""
+    f = tempfile.NamedTemporaryFile()
+    filename = f.name
+    async with aiosqlite.connect(filename) as connection:
+        # add data to sqlite
+        await add_data(
+            connection,
+            data="""
+                MONDO:0005148(( category biolink:Disease ))
+                MONDO:0005148<-- predicate biolink:treats --CHEBI:6801
+                CHEBI:6801(( category biolink:ChemicalSubstance ))
+            """
+        )
+    async with KnowledgeProvider(filename) as kp:
+        qgraph = {
+            "nodes": {
+                "n0": {
+                    "categories": ["biolink:ChemicalSubstance"],
+                },
+                "n1": {
+                    "categories": ["biolink:Disease"],
+                    "ids": ["MONDO:0005148"],
+                },
+            },
+            "edges": {
+                "e01": {
+                    "subject": "n0",
+                    "object": "n1",
+                    "predicates": ["biolink:treats"],
+                },
+            },
+        }
+        kgraph, results = await kp.get_results(qgraph)
+        assert results
