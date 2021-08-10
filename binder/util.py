53f34cb0241781d4d8ed7293b24745199c4772ab
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-08-10 09:23:10 -0400

    Remove unused to_list()

--- binder/util.py
+++ binder/util.py
@@ -39,13 +39,6 @@ def is_symmetric(predicate):
     return el.symmetric
 
 
-def to_list(scalar_or_list):
-    """Enclose in list if necessary."""
-    if not isinstance(scalar_or_list, list):
-        return [scalar_or_list]
-    return scalar_or_list
-
-
 class NoAnswersException(Exception):
     """No answers to question."""
 
