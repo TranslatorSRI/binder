6e8cb8f10e33d4e59e84626fad67d38d0940c6b7
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-09-01 09:31:21 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="binder",
-    version="4.2.4",
+    version="4.3.0",
     author="Patrick Wang",
     author_email="patrick@covar.com",
     url="https://github.com/TranslatorSRI/binder",
